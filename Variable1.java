518c7d6f2c9ae3348518f7ee1c9c5051759dcd73
NithyaSanthoshini2106 <nithyasanthoshini099@gmail.com>
2025-10-09 10:20:55 +0530

    java

---
+++ Variable1.java
@@ -0,0 +1,15 @@
+
+
+
+public class Variable1 {
+    public static void main(String [] args) {
+        int a = 10 , b=20;
+
+        int temp;
+        temp = a;
+        a = b;
+        b = temp;
+        System.out.println(a + b);
+    }
+
+}
