7898de94bc7df84149d3d01546731ecc1f034907
NithyaSanthoshini2106 <nithyasanthoshini099@gmail.com>
2025-10-10 14:49:15 +0530

    ok

---
+++ Variable3.java
@@ -0,0 +1,16 @@
+
+
+
+public class Variable3 {
+    public static void main (String [] args) {
+        int a=98765432, b= 1234567;
+
+        a= a+b;
+        b=a-b;
+        a=a-b;
+        System.out.println(a + b);
+
+
+    }
+    
+}
---
+++ Variable4.java
@@ -0,0 +1,15 @@
+
+
+public class Variable4 {
+    public static void main (String [] args) {
+        int a=2345657, b= 345678567;
+
+        a= a+b;
+        b=a-b;
+        a=a-b;
+        System.out.println(a + b );
+
+
+    }
+    
+}
