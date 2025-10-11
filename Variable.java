e48a0a700bba4b1d52d38eef307863c830650777
NithyaSanthoshini2106 <nithyasanthoshini099@gmail.com>
2025-10-11 15:12:58 +0530

    ok

---
+++ Variable.java
@@ -0,0 +1,11 @@
+
+
+
+public class Variable {
+    public static void main(String[] args) {
+        double principal = 1078657, rate = 556789, time = 245678;
+        double si = (principal * rate * time) / 100;
+
+        System.out.println(si);
+    }
+}
