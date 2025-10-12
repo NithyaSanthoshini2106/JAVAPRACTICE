9a51128f4a06325e572b5ee9a5e30d3e05ae6b7f
NithyaSanthoshini2106 <nithyasanthoshini099@gmail.com>
2025-10-12 10:31:42 +0530

    ok

---
+++ Variable5.java
@@ -0,0 +1,14 @@
+
+
+
+public class Variable5{
+    public static void main(String[] args) {
+        int a = 156789876, b = 267865435;
+
+        if (a > b) {
+            System.out.println(a);
+        } else {
+            System.out.println(b);
+        }
+    }
+}
---
+++ Variables/one.java
@@ -0,0 +1,12 @@
+
+public class one {
+    public static void main(String[] args) {
+        int a = 15, b = 25;
+
+        if (a > b) {
+            System.out.println(a );
+        } else {
+            System.out.println(b );
+        }
+    }
+}
