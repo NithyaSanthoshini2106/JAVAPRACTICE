731bceb02172858141bfbb3f49416a7493591afb
NithyaSanthoshini2106 <nithyasanthoshini099@gmail.com>
2025-10-17 09:39:28 +0530

    ok

---
+++ Variables/five.java
@@ -0,0 +1,12 @@
+
+
+
+
+public class five {
+    public static void main(String[] args) {
+        double celsius = 37;
+        double fahrenheit = (celsius * 9/5) + 32;
+
+        System.out.println(celsius + "°C = " + fahrenheit + "°F");
+    }
+}
