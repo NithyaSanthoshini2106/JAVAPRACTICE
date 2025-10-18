791dc88c60bce5d289f4a6309aba97d3dae37339
NithyaSanthoshini2106 <nithyasanthoshini099@gmail.com>
2025-10-18 09:56:08 +0530

    ok

---
+++ Variables/six.java
@@ -0,0 +1,11 @@
+
+
+
+public class six {
+    public static void main(String[] args) {
+        int length = 10, width = 5;
+        int perimeter = 2 * (length + width);
+
+        System.out.println("Perimeter of Rectangle = " + perimeter);
+    }
+}
