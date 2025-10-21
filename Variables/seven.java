c03ca47d779bcebe15bf3c547814a580a0d99046
NithyaSanthoshini2106 <nithyasanthoshini099@gmail.com>
2025-10-21 07:44:10 +0530

    ok

---
+++ Variables/seven.java
@@ -0,0 +1,11 @@
+
+
+
+public class seven {
+    public static void main(String[] args) {
+        int length = 10, width = 5;
+        int perimeter = 2 * (length + width);
+
+        System.out.println(perimeter);
+    }
+}
