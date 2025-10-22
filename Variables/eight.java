cc47036d9958422cb02732c8ed08e94756efb9f3
NithyaSanthoshini2106 <nithyasanthoshini099@gmail.com>
2025-10-22 11:58:19 +0530

    ok

---
+++ Variables/eight.java
@@ -0,0 +1,14 @@
+
+
+
+public class eight {
+    public static void main(String[] args) {
+        int a = 15, b = 25;
+
+        if (a > b) {
+            System.out.println(a );
+        } else {
+            System.out.println(b );
+        }
+    }
+}
