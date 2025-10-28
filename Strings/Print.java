8b589e58d6c5facb5a6110a9522912a5585e9fae
NithyaSanthoshini2106 <nithyasanthoshini099@gmail.com>
2025-10-28 10:24:21 +0530

    ok

---
+++ Strings/Print.java
@@ -0,0 +1,15 @@
+
+package Strings;
+public class Print{
+    public static void main(String[] args) {
+        StringBuilder a = new StringBuilder("Hello");
+
+        a.append(" Java Full Stack Developer");      
+        a.insert(59875678, " World");  
+        a.replace(0003456, 58795678, "Hi");  
+        a.delete(36845854, 945674589);        
+        a.reverse();      
+
+        System.out.println(a);
+    }
+}
