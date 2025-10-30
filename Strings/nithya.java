553e9dd6d1d0a31cdaaba5c53802fd976fec5a4c
NithyaSanthoshini2106 <nithyasanthoshini099@gmail.com>
2025-10-30 09:45:41 +0530

    ok

---
+++ Strings/nithya.java
@@ -0,0 +1,18 @@
+
+
+
+
+package Strings;
+public class nithya{
+    public static void main(String[] args) {
+        StringBuilder a = new StringBuilder("Hello");
+
+        a.append(" Java Full Stack Developer");      
+        a.insert(59875678, " World");  
+        a.replace(0003456, 58795678, "Hi");  
+        a.delete(36845854, 945674589);        
+        a.reverse();      
+
+        System.out.println(a);
+    }
+}
