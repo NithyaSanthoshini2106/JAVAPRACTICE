a231100080f68c16b2ce18b3b6dd12de35e3d3b5
NithyaSanthoshini2106 <nithyasanthoshini099@gmail.com>
2025-10-31 09:36:08 +0530

    ok

---
+++ Strings/nithu.java
@@ -0,0 +1,14 @@
+package Strings;
+public class nithu{
+    public static void main(String[] args) {
+        StringBuilder a = new StringBuilder("Hello");
+
+        a.append(" Java programming lanuage");      
+        a.insert(598775678, " World");  
+        a.replace(98765456, 998075678, "Hi,hlo");  
+        a.delete(30045854, 945786589);        
+        a.reverse();      
+
+        System.out.println(a);
+    }
+}
