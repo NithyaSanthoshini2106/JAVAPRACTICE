8d813c6ceb273461f606ca049146618fdb4325e9
NithyaSanthoshini2106 <nithyasanthoshini099@gmail.com>
2025-11-03 10:31:31 +0530

    ok

---
+++ practice codes/three.java
@@ -0,0 +1,18 @@
+
+
+
+public class three {
+    public static void main(String[] args) {
+        String str = "Nithya";
+        String rev = "Spoorthi";
+        
+        for (int i = str.length() - 1; i >= 0; i--) {
+            rev += str.charAt(i);
+        }
+        
+        if (str.equals(rev))
+            System.out.println(str + " is a palindrome");
+        else
+            System.out.println(str + " is not a palindrome");
+    }
+}
