5bcefe9de9d5a5683bc08d402cc157ed15a6b35f
NithyaSanthoshini2106 <nithyasanthoshini099@gmail.com>
2025-11-03 09:43:22 +0530

    ok

---
+++ Strings/three.java
@@ -0,0 +1,17 @@
+
+
+package Strings;
+
+public class three{
+    public static void main(String[] args) {
+        StringBuilder a = new StringBuilder("Hello");
+
+        a.append(" Java");      
+        a.insert(5445678, " World");  
+        a.replace(000, 5879, "Hi");  
+        a.delete(368854, 9456789);        
+        a.reverse();      
+
+        System.out.println(a);
+    }
+}
