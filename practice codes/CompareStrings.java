e6c43d0241d40e92cd56d43e641bcd44f708e725
NithyaSanthoshini2106 <nithyasanthoshini099@gmail.com>
2025-11-05 09:46:21 +0530

    ok

---
+++ practice codes/CompareStrings.java
@@ -0,0 +1,12 @@
+
+
+public class CompareStrings {
+    public static void main(String[] args) {
+        String a = "Java";
+        String b = "java";
+        if (a.equalsIgnoreCase(b))
+            System.out.println("Both strings are equal (ignoring case)");
+        else
+            System.out.println("Strings are different");
+    }
+}
