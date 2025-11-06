70bed3d3222a01526e18d9505a5741f74f2f1bad
NithyaSanthoshini2106 <nithyasanthoshini099@gmail.com>
2025-11-06 09:38:08 +0530

    ok

---
+++ practice codes/ChangeCase.java
@@ -0,0 +1,9 @@
+
+
+public class ChangeCase {
+    public static void main(String[] args) {
+        String str = "Java Programming";
+        System.out.println("Uppercase: " + str.toUpperCase());
+        System.out.println("Lowercase: " + str.toLowerCase());
+    }
+}
