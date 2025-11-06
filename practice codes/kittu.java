f1a12f9c56db2811ad821031a6b0d08404e260fc
NithyaSanthoshini2106 <nithyasanthoshini099@gmail.com>
2025-11-06 09:39:25 +0530

    ok

---
+++ practice codes/kittu.java
@@ -0,0 +1,10 @@
+
+
+
+public class kittu {
+    public static void main(String[] args) {
+        String str = "Java Programming";
+        System.out.println("Uppercase: " + str.toUpperCase());
+        System.out.println("Lowercase: " + str.toLowerCase());
+    }
+}
