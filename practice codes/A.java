32291e521634113a96555fc43efd727ee5172437
NithyaSanthoshini2106 <nithyasanthoshini099@gmail.com>
2025-11-07 10:21:12 +0530

    ok

---
+++ practice codes/A.java
@@ -0,0 +1,10 @@
+public class A{
+    public static void main(String[] args) {
+        String a = "Java";
+        String b = "java";
+        if (a.equalsIgnoreCase(b))
+            System.out.println("Both strings are equal (ignoring case)");
+        else
+            System.out.println("Strings are different");
+    }
+}
