10bf84638649e86ffdf99a2811f4e3c1faa05fd0
NithyaSanthoshini2106 <nithyasanthoshini099@gmail.com>
2025-11-08 13:14:42 +0530

    ok

---
+++ practice codes/B.java
@@ -0,0 +1,10 @@
+public class B {
+    
+    
+    public static void main(String[] args) {
+        String str = "Java Programming";
+        System.out.println("Length: " + str.length());
+    }
+}
+
+
