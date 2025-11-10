32239b5b64a2fa4676529be62ddddfbe9a677165
NithyaSanthoshini2106 <nithyasanthoshini099@gmail.com>
2025-11-10 09:39:39 +0530

    ok

---
+++ practice codes/C.java
@@ -0,0 +1,20 @@
+
+
+
+
+
+public class C{
+    public static void main(String[] args) {
+        String a = "NithyaRaju";
+        String b = "";
+        
+        for (int i = a.length() - 1; i >= 0; i--) {
+            b += a.charAt(i);
+        }
+
+        if (a.equals(b))
+            System.out.println(a + " is a palindrome");
+        else
+            System.out.println(a + " is not a palindrome");
+    }
+}
