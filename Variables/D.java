e82317f4468d422a67afc6e19f27c95ff9683635
NithyaSanthoshini2106 <nithyasanthoshini099@gmail.com>
2025-11-11 09:36:05 +0530

    ok

---
+++ Variables/D.java
@@ -0,0 +1,13 @@
+
+
+
+public class D {
+    public static void main(String[] args) {
+        String a = "Java";
+        String b = "java";
+        if (a.equalsIgnoreCase(b))
+            System.out.println("Both strings are equal (ignoring case)");
+        else
+            System.out.println("Strings are different");
+    }
+}
