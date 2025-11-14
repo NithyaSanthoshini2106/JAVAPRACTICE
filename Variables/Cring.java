826fe67bd8dccd20192bc4a256b4123e70a8d639
NithyaSanthoshini2106 <nithyasanthoshini099@gmail.com>
2025-11-14 09:42:54 +0530

    ok

---
+++ Variables/Cring.java
@@ -0,0 +1,11 @@
+
+
+
+public class Cring {
+    public static void main(String[] args) {
+        String first = "Nithya";
+        String last = "Santhoshini";
+        String fullName = first +last;
+        System.out.println(fullName);
+    }
+}
