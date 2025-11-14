5306e0fade3a6081eac62b8f5c90d88a86523fbd
NithyaSanthoshini2106 <nithyasanthoshini099@gmail.com>
2025-11-14 09:44:21 +0530

    ok

---
+++ practice codes/ConcatString.java
@@ -0,0 +1,10 @@
+
+
+public class ConcatString {
+    public static void main(String[] args) {
+        String first = "Nithya Adluru";
+        String last = "Santhoshini Adluru";
+        String fullName = first + last;
+        System.out.println(fullName);
+    }
+}
