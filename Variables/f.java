8c39363a00c05079db5e8f5d2d387fe67c3e2524
NithyaSanthoshini2106 <nithyasanthoshini099@gmail.com>
2025-11-18 18:15:34 +0530

    ok

---
+++ Variables/f.java
@@ -0,0 +1,13 @@
+
+
+public class f{
+    public static void main(String[] args) {
+        int num = 1156782290;
+
+        if (num % 2 == 0) {
+            System.out.println(num);
+        } else {
+            System.out.println(num);
+        }
+    }
+}
