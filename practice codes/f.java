6e555a8380c18d161b5f2471b5ba5bc93416a653
NithyaSanthoshini2106 <nithyasanthoshini099@gmail.com>
2025-11-18 18:17:21 +0530

    ok

---
+++ practice codes/f.java
@@ -0,0 +1,13 @@
+
+
+public class f {
+    public static void main(String[] args) {
+        int num = 11567890;
+
+        if (num % 2 == 0) {
+            System.out.println(num);
+        } else {
+            System.out.println(num);
+        }
+    }
+}
