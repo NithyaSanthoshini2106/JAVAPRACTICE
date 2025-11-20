3f6aa6422d54accf3bedcf030db6772d70915757
NithyaSanthoshini2106 <nithyasanthoshini099@gmail.com>
2025-11-20 09:40:38 +0530

    ok

---
+++ Variables/H.java
@@ -0,0 +1,11 @@
+
+
+
+
+public class H {
+    public static void main(String[] args) {
+        String str = "Java Programming Language";
+        System.out.println(str.toUpperCase());
+        System.out.println(str.toLowerCase());
+    }
+}
