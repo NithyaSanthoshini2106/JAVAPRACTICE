f22333488b750ba7fca65f52c1790a6403e753f2
NithyaSanthoshini2106 <nithyasanthoshini099@gmail.com>
2025-11-20 09:42:58 +0530

    ok

---
+++ practice codes/H.java
@@ -0,0 +1,10 @@
+
+
+
+public class H {
+    public static void main(String[] args) {
+        String str = "Java Full Stack Programming Language";
+        System.out.println(str.toUpperCase());
+        System.out.println(str.toLowerCase());
+    }
+}
