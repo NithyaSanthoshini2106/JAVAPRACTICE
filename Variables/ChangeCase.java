3687064afa78565eb75d7e9dfdc9c9ba17bf7e44
NithyaSanthoshini2106 <nithyasanthoshini099@gmail.com>
2025-11-24 09:37:59 +0530

    ok

---
+++ Variables/ChangeCase.java
@@ -0,0 +1,12 @@
+
+
+
+
+
+public class ChangeCase {
+    public static void main(String[] args) {
+        String str = "Java Programming languagea";
+        System.out.println(str.toUpperCase());
+        System.out.println(str.toLowerCase());
+    }
+}
