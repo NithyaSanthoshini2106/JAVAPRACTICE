4e5bb4b0697f2de94787bb1f218caf0e9307dd95
NithyaSanthoshini2106 <nithyasanthoshini099@gmail.com>
2025-11-25 09:34:02 +0530

    ok

---
+++ practice codes/J.java
@@ -0,0 +1,16 @@
+
+
+
+
+
+
+public class  J{
+    public static void main(String[] args) {
+        String str = "NithyaSpoorthinithuraju Adluru ";
+        String rev = "";
+        for (int i = str.length() - 1; i >= 0; i--) {
+            rev += str.charAt(i);
+        }
+        System.out.println(rev);
+    }
+}
