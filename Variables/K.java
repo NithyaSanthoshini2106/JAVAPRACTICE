561be2a24a2397b405835408cce6852f61f32b6f
NithyaSanthoshini2106 <nithyasanthoshini099@gmail.com>
2025-11-26 09:31:17 +0530

    ok

---
+++ Variables/K.java
@@ -0,0 +1,14 @@
+
+
+
+
+
+
+public class K {
+    public static void main(String[] args) {
+        double celsius = 47;
+        double fahrenheit = (celsius * 9/5) + 32;
+
+        System.out.println(celsius + fahrenheit);
+    }
+}
