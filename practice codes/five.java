57a70947924aeafeb2ab956e867ff95fa361b9d7
NithyaSanthoshini2106 <nithyasanthoshini099@gmail.com>
2025-11-26 09:32:36 +0530

    ok

---
+++ practice codes/five.java
@@ -0,0 +1,12 @@
+
+
+
+
+public class five {
+    public static void main(String[] args) {
+        double celsius = 27;
+        double fahrenheit = (celsius * 9/5) + 32;
+
+        System.out.println(celsius + fahrenheit );
+    }
+}
