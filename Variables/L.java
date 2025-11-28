739b5a7b7923dc97084e9dc7373855a1555b0cef
NithyaSanthoshini2106 <nithyasanthoshini099@gmail.com>
2025-11-28 09:33:46 +0530

    ok

---
+++ Variables/L.java
@@ -0,0 +1,15 @@
+
+
+
+
+
+
+
+public class L {
+    public static void main(String[] args) {
+        double celsius = 27;
+        double fahrenheit = (celsius * 4/5) + 32;
+
+        System.out.println(celsius + fahrenheit );
+    }
+}
