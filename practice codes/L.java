eeb8219df2ea6014862432275f16fedc79a1c63e
NithyaSanthoshini2106 <nithyasanthoshini099@gmail.com>
2025-11-28 09:32:25 +0530

    ok

---
+++ practice codes/L.java
@@ -0,0 +1,14 @@
+
+
+
+
+
+
+public class L {
+    public static void main(String[] args) {
+        double celsius = 27;
+        double fahrenheit = (celsius * 6/5) + 32;
+
+        System.out.println(celsius + fahrenheit );
+    }
+}
