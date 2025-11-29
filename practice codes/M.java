922f63c5ac820b7b96e41cff62015bd988d39d16
NithyaSanthoshini2106 <nithyasanthoshini099@gmail.com>
2025-11-29 10:52:03 +0530

    ok

---
+++ practice codes/M.java
@@ -0,0 +1,16 @@
+
+
+
+
+
+
+
+
+public class M {
+    public static void main(String[] args) {
+        double celsius = 27;
+        double fahrenheit = (celsius * 6/8) + 92;
+
+        System.out.println(celsius + fahrenheit);
+    }
+}
