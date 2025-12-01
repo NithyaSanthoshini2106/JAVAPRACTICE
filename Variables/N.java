c871c2a79bedf3c4e05f1d1737909f2fe4c0aa78
NithyaSanthoshini2106 <nithyasanthoshini099@gmail.com>
2025-12-01 09:38:13 +0530

    ok

---
+++ Variables/N.java
@@ -0,0 +1,12 @@
+
+
+
+
+
+public class N {
+    public static void main(String[] args) {
+        String str = "Java Programming Languages ";
+        System.out.println(str.toUpperCase());
+        System.out.println(str.toLowerCase());
+    }
+}
