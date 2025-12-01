6df95d077bf0dbb41b976f791f8917e3c8a49042
NithyaSanthoshini2106 <nithyasanthoshini099@gmail.com>
2025-12-01 09:32:28 +0530

    ok

---
+++ practice codes/N.java
@@ -0,0 +1,10 @@
+
+
+
+public class N {
+    public static void main(String[] args) {
+        String str = "Java Programming Languages ";
+        System.out.println(str.toUpperCase());
+        System.out.println(str.toLowerCase());
+    }
+}
