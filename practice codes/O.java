7b7c86e7acaacf3e28a939e085167551f226b23e
NithyaSanthoshini2106 <nithyasanthoshini099@gmail.com>
2025-12-02 12:35:28 +0530

    ok

---
+++ practice codes/O.java
@@ -0,0 +1,14 @@
+
+
+
+
+public class O {
+    public static void main(String[] args) {
+        String str = "spoorthiRaju Adluru";
+        String rev = "";
+        for (int i = str.length() - 1; i >= 0; i--) {
+            rev += str.charAt(i);
+        }
+        System.out.println(rev);
+    }
+}
