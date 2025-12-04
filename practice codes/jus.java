07489d074c42d1c0e707394bcc25f631413d176d
NithyaSanthoshini2106 <nithyasanthoshini099@gmail.com>
2025-12-04 19:05:25 +0530

    ok

---
+++ practice codes/jus.java
@@ -0,0 +1,16 @@
+
+
+
+public class jus {
+    public static void main (String [] args) {
+        int a=2345, b= 345678;
+
+        a= a+b;
+        b=a-b;
+        a=a-b;
+        System.out.println(a + b);
+
+
+    }
+    
+}
