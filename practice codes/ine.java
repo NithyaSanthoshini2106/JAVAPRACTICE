232878662a503b49ba2bc640a2eb39fb5faba0f8
NithyaSanthoshini2106 <nithyasanthoshini099@gmail.com>
2025-12-06 10:15:01 +0530

    ok

---
+++ practice codes/ine.java
@@ -0,0 +1,18 @@
+
+
+
+
+
+
+public class ine {
+    public static void main(String[] args) {
+        String str = "Java";
+        StringBuilder a = new StringBuilder(str);
+        String b = a.toString();
+        StringBuffer c = new StringBuffer(str);
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
