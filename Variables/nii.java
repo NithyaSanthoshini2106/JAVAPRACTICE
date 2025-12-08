68d93ef608c290091693c67af58499c227456211
NithyaSanthoshini2106 <nithyasanthoshini099@gmail.com>
2025-12-08 09:54:39 +0530

    ok

---
+++ Variables/nii.java
@@ -0,0 +1,11 @@
+
+
+
+public class nii {
+    public static void main(String[] args) {
+        double celsius = 27;
+        double fahrenheit = (celsius * 9/5) + 32;
+
+        System.out.println(celsius + fahrenheit );
+    }
+}
