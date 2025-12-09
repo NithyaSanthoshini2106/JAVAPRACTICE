9594dabb95dab91f3b22c5ee61c2802ec665d5e7
NithyaSanthoshini2106 <nithyasanthoshini099@gmail.com>
2025-12-09 09:27:13 +0530

    ok

---
+++ practice codes/spoorthi.java
@@ -0,0 +1,18 @@
+
+
+
+
+
+public class spoorthi {
+    public static void main (String [] args) {
+        int a=2345, b= 345678;
+
+        a= 100;
+        b= 200;
+        a= 300;
+        System.out.println(a + b);
+
+
+    }
+    
+}
