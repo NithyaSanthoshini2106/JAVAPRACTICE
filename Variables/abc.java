8ed4d475a5e9c0e798f31fc6898eb784a9e3d3f4
NithyaSanthoshini2106 <nithyasanthoshini099@gmail.com>
2025-12-10 17:37:30 +0530

    ok

---
+++ Variables/abc.java
@@ -0,0 +1,13 @@
+
+
+
+
+
+
+public class abc {
+    public static void main(String[] args) {
+        String str = "Java Programming languages on Full Stack development ";
+        System.out.println(str.toUpperCase());
+        System.out.println(str.toLowerCase());
+    }
+}
