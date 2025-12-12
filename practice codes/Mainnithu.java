638a0575907575bc0969974ac889d28ad579fedc
NithyaSanthoshini2106 <nithyasanthoshini099@gmail.com>
2025-12-12 09:42:16 +0530

    ok

---
+++ practice codes/Mainnithu.java
@@ -0,0 +1,25 @@
+
+
+
+
+
+class Student {
+    String name;
+    int age;
+
+    Student(String name, int age){
+        this.name = name;
+        this.age = age;
+    }
+
+    void display(){
+        System.out.println(name + age);
+    }
+}
+
+public class Mainnithu {
+    public static void main(String[] args){
+        Student s = new Student("Nithya_Spoorthi_VarmaRaju", 25);
+        s.display();
+    }
+}
