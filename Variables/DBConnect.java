1bfd25d4d07b2fc5d00c74e523bb80b0eee3f13d
NithyaSanthoshini2106 <nithyasanthoshini099@gmail.com>
2025-12-12 09:50:29 +0530

    ok

---
+++ Variables/DBConnect.java
@@ -0,0 +1,18 @@
+
+
+import java.sql.*;
+
+public class DBConnect {
+    public static void main(String[] args) {
+        try {
+            Connection con = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/testdb", "root", "password"
+            );
+
+            System.out.println("Connected!");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
+
