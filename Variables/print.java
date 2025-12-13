3fcdaac8e199774761b61ce3922b023e68c38308
NithyaSanthoshini2106 <nithyasanthoshini099@gmail.com>
2025-12-13 15:37:53 +0530

    ok

---
+++ Variables/print.java
@@ -0,0 +1,17 @@
+
+
+
+
+
+
+
+public class print {
+    public static void main(String[] args) {
+        String a = "Java programming languages";
+        String b = "python programming languages";
+        if (a.equalsIgnoreCase(b))
+            System.out.println("Both strings are equal (ignoring case)");
+        else
+            System.out.println("Strings are different");
+    }
+}
