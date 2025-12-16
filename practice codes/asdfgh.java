df68d36a740d0eebd68e42c97656f49e1ec36ff4
NithyaSanthoshini2106 <nithyasanthoshini099@gmail.com>
2025-12-16 09:42:48 +0530

    ok

---
+++ practice codes/asdfgh.java
@@ -0,0 +1,14 @@
+public class asdfgh {
+    
+    public static void main(String [] args) {
+        int a = 1078643 , b=2057897654;
+
+        int temp;
+        temp = a;
+        a = b;
+        b = temp;
+        System.out.println(a + b);
+    }
+
+}
+    
