83bb9643ec072bbcffe6364587e7e2c4b06462e1
NithyaSanthoshini2106 <nithyasanthoshini099@gmail.com>
2025-12-17 15:40:51 +0530

    ok

---
+++ Variables/tyyu.java
@@ -0,0 +1,12 @@
+public class tyyu {
+
+    public static void main(String[] args) {
+        String a = "Java Languages";
+        String b = "java Developers";
+        if (a.equalsIgnoreCase(b))
+            System.out.println("Both strings are equal (ignoring case)");
+        else
+            System.out.println("Strings are different");
+    }
+}
+
