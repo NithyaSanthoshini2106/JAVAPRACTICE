4d293334465859217ffe70020d50da73ca4d57da
NithyaSanthoshini2106 <nithyasanthoshini099@gmail.com>
2025-12-17 15:38:40 +0530

    ok

---
+++ practice codes/yuiop.java
@@ -0,0 +1,10 @@
+public class yuiop {
+    public static void main(String[] args) {
+        String a = "Java Programming Languages";
+        String b = "Python Programming Languages";
+        if (a.equalsIgnoreCase(b))
+            System.out.println("Both strings are equal (ignoring case)");
+        else
+            System.out.println("Strings are different");
+    }
+}
