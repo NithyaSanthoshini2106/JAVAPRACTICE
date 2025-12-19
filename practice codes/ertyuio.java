a19d1162dc750bfbb873267e01b1f485d41310c0
NithyaSanthoshini2106 <nithyasanthoshini099@gmail.com>
2025-12-19 09:57:02 +0530

    ok

---
+++ practice codes/ertyuio.java
@@ -0,0 +1,15 @@
+public class ertyuio {
+
+
+    public static void main(String[] args) {
+        String a = "Java a,b,c,d...";
+        String b = "java 1,2,3,4,....";
+        if (a.equalsIgnoreCase(b))
+            System.out.println("Both strings are equal (ignoring case)");
+        else
+            System.out.println("Strings are different");
+    }
+}
+ 
+
+
