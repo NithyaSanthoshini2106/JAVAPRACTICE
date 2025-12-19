5ddd9dfe06b3980529379971f70835354c022059
NithyaSanthoshini2106 <nithyasanthoshini099@gmail.com>
2025-12-19 09:56:06 +0530

    ok

---
+++ Variables/codes/B.java
@@ -0,0 +1,15 @@
+package codes;
+
+public class B {
+
+    public static void main(String[] args) {
+        String a = "Java a,b,c,d,e,f...";
+        String b = "java 1,2,3,4,5,6,....";
+        if (a.equalsIgnoreCase(b))
+            System.out.println("Both strings are equal (ignoring case)");
+        else
+            System.out.println("Strings are different");
+    }
+}
+ 
+
