279f192722e7f314749c9e5cc25c76dd33fb1514
NithyaSanthoshini2106 <nithyasanthoshini099@gmail.com>
2025-12-20 12:07:09 +0530

    ok

---
+++ Variables/uuuu.java
@@ -0,0 +1,9 @@
+
+
+public class uuuu {
+    public static void main(String[] args) {
+        String str = "python and java Programming languages on Full Stack development ";
+        System.out.println(str.toUpperCase());
+        System.out.println(str.toLowerCase());
+    }
+}
