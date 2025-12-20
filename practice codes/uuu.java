8bc23ebc657b44a1eca824d9be5a44b71da8a118
NithyaSanthoshini2106 <nithyasanthoshini099@gmail.com>
2025-12-20 12:07:53 +0530

    ok

---
+++ practice codes/uuu.java
@@ -0,0 +1,10 @@
+public class uuu {
+    
+    public static void main(String[] args) {
+        String str = "Java Programming languages on Full Stack development ";
+        System.out.println(str.toUpperCase());
+        System.out.println(str.toLowerCase());
+    }
+}
+
+
