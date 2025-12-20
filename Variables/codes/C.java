fb7e585a9ebb3512866a185bb22bccaf57ff1be2
NithyaSanthoshini2106 <nithyasanthoshini099@gmail.com>
2025-12-20 12:08:53 +0530

    ok

---
+++ Variables/codes/C.java
@@ -0,0 +1,12 @@
+package codes;
+
+public class C {
+   
+    public static void main(String[] args) {
+        String str = "Java and python Programming languages on Full Stack development ";
+        System.out.println(str.toUpperCase());
+        System.out.println(str.toLowerCase());
+    }
+}
+
+
