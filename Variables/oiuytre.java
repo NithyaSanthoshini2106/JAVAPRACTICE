623c5d844c55512e2e674f3b02c3398b65f3989f
NithyaSanthoshini2106 <nithyasanthoshini099@gmail.com>
2025-12-21 21:09:09 +0530

    ok

---
+++ Variables/oiuytre.java
@@ -0,0 +1,16 @@
+public class oiuytre {
+    
+    public static void main (String [] args) {
+        int a=98977656, b= 7876544;
+
+        a= a+b;
+        b=a-b;
+        a=a-b;
+        System.out.println(a + b);
+
+
+    }
+    
+}
+
+
