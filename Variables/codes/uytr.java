bb7fe2e7808281fff6f1724f6eeed08848096ee9
NithyaSanthoshini2106 <nithyasanthoshini099@gmail.com>
2025-12-21 21:06:49 +0530

    ok

---
+++ Variables/codes/uytr.java
@@ -0,0 +1,18 @@
+package codes;
+
+public class uytr {
+   
+    public static void main (String [] args) {
+        int a=234545678, b= 345678345;
+
+        a= a+b;
+        b=a-b;
+        a=a-b;
+        System.out.println(a + b);
+
+
+    }
+    
+}
+
+
