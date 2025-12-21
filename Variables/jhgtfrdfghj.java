c013a83ccd8249aa58400854ff6b9b4db5969a3b
NithyaSanthoshini2106 <nithyasanthoshini099@gmail.com>
2025-12-21 21:07:51 +0530

    ok

---
+++ Variables/jhgtfrdfghj.java
@@ -0,0 +1,16 @@
+public class jhgtfrdfghj {
+  
+    public static void main (String [] args) {
+        int a=2345567, b= 345678987;
+
+        a= a+b;
+        b=a-b;
+        a=a-b;
+        System.out.println(a + b);
+
+
+    }
+    
+}
+
+
