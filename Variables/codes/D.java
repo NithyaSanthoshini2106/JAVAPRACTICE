036119ac1c7562155ea6db43e3fb48c7fa853c62
NithyaSanthoshini2106 <nithyasanthoshini099@gmail.com>
2025-12-22 12:44:14 +0530

    ok

---
+++ Variables/codes/D.java
@@ -0,0 +1,16 @@
+package codes;
+
+public class D {
+    
+
+    public static void main(String[] args) {
+        String str = "Nithya Spoorthi Varma";
+        String rev = "";
+        for (int i = str.length() - 1; i >= 0; i--) {
+            rev += str.charAt(i);
+        }
+        System.out.println(rev);
+    }
+}
+
+
