fdd56cb4563bc869746eacc32746f79046fe23b7
NithyaSanthoshini2106 <nithyasanthoshini099@gmail.com>
2025-12-22 12:43:11 +0530

    java

---
+++ practice codes/lav.java
@@ -0,0 +1,14 @@
+public class lav {
+    
+
+    public static void main(String[] args) {
+        String str = "Nithya Spoorthi Raju";
+        String rev = "";
+        for (int i = str.length() - 1; i >= 0; i--) {
+            rev += str.charAt(i);
+        }
+        System.out.println(rev);
+    }
+}
+
+
