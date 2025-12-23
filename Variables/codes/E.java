2b81970504a7efb1d196059ae13239457052d622
NithyaSanthoshini2106 <nithyasanthoshini099@gmail.com>
2025-12-23 09:28:33 +0530

    ok

---
+++ Variables/codes/E.java
@@ -0,0 +1,11 @@
+package codes;
+
+public class E {
+    public static void main(String[] args) {
+        double celsius = 279008765;
+        double fahrenheit = (celsius * 569/875) + 878798932;
+
+        System.out.println(celsius + fahrenheit );
+    }
+}
+
