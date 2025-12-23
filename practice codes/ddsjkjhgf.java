eab6503f685d2066e765ec23bb4ac045e6c70052
NithyaSanthoshini2106 <nithyasanthoshini099@gmail.com>
2025-12-23 09:30:30 +0530

    ok

---
+++ practice codes/ddsjkjhgf.java
@@ -0,0 +1,10 @@
+public class ddsjkjhgf {
+    public static void main(String[] args) {
+        double celsius = 674893093;
+        double fahrenheit = (celsius * 4339/435) + 987632;
+
+        System.out.println(celsius + fahrenheit );
+    }
+}
+
+
