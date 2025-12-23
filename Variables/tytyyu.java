57e8657567bcab07072136c3a61359ef2ed9bde7
NithyaSanthoshini2106 <nithyasanthoshini099@gmail.com>
2025-12-23 09:27:25 +0530

    ok:

---
+++ Variables/tytyyu.java
@@ -0,0 +1,11 @@
+public class tytyyu {
+
+    public static void main(String[] args) {
+        double celsius = 2787676;
+        double fahrenheit = (celsius * 29/5) + 8732;
+
+        System.out.println(celsius + fahrenheit );
+    }
+}
+
+
