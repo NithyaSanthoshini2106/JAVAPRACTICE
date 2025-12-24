a2a6bb64a6f09146ad28823cd4096de50f154117
NithyaSanthoshini2106 <nithyasanthoshini099@gmail.com>
2025-12-24 19:22:41 +0530

    ok

---
+++ Variables/codes/F.java
@@ -0,0 +1,11 @@
+
+
+
+
+public class F {
+    public static void main(String[] args) {
+        String str = "Java and Python Programming languageson the Full Stack development ";
+        System.out.println(str.toUpperCase());
+        System.out.println(str.toLowerCase());
+    }
+}
