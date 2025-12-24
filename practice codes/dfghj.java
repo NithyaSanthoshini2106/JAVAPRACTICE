9f2b6fe5a1be56959a17a1430a6b1c9a011fe512
NithyaSanthoshini2106 <nithyasanthoshini099@gmail.com>
2025-12-24 19:21:18 +0530

    ok

---
+++ practice codes/dfghj.java
@@ -0,0 +1,11 @@
+
+
+
+
+public class dfghj {
+    public static void main(String[] args) {
+        String str = "java and python programming languages";
+        System.out.println(str.toUpperCase());
+        System.out.println(str.toLowerCase());
+    }
+}
---
+++ practice codes/jhg.java
@@ -0,0 +1,11 @@
+
+
+
+
+public class jhg {
+    public static void main(String[] args) {
+        String str = "Java Programming languages on Full Stack development ";
+        System.out.println(str.toUpperCase());
+        System.out.println(str.toLowerCase());
+    }
+}
