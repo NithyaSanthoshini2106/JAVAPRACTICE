231be82ef2df09c2497cb9622c53e1d788ff40bf
NithyaSanthoshini2106 <nithyasanthoshini099@gmail.com>
2025-12-27 16:05:04 +0530

    ok

---
+++ Variables/bnbv.java
@@ -0,0 +1,9 @@
+public class bnbv {
+    
+    public static void main(String[] args) {
+        String str = "abcdefghijkl";
+        System.out.println(str.toUpperCase());
+        System.out.println(str.toLowerCase());
+    }
+}
+
