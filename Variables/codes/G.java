4c59189d5d9240d26dc8afbf17d0e146397a53ae
NithyaSanthoshini2106 <nithyasanthoshini099@gmail.com>
2025-12-27 16:06:35 +0530

    ok

---
+++ Variables/codes/G.java
@@ -0,0 +1,11 @@
+package codes;
+
+public class G {
+    
+    public static void main(String[] args) {
+        String str = "Java Programming And Python Programming";
+        System.out.println(str.toUpperCase());
+        System.out.println(str.toLowerCase());
+    }
+}
+
