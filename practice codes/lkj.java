46a76a43f3a1f85b5f622a783e987a9c5ef27fb8
NithyaSanthoshini2106 <nithyasanthoshini099@gmail.com>
2025-12-27 16:03:39 +0530

    ok

---
+++ practice codes/lkj.java
@@ -0,0 +1,8 @@
+public class lkj {
+    public static void main(String[] args) {
+        String str = "0987654323545687";
+        System.out.println(str.toUpperCase());
+        System.out.println( str.toLowerCase());
+    }
+}
+
