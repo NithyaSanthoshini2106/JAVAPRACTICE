b9d4da53b15a4fae3e6d55f18a51a6bc1d784877
NithyaSanthoshini2106 <nithyasanthoshini099@gmail.com>
2025-12-30 12:08:10 +0530

    kkk

---
+++ practice codes/BankApp.java
@@ -0,0 +1,40 @@
+
+
+import java.util.Scanner;
+
+class BankAccount {
+
+    double balance = 6787890;
+
+    void deposit(double amount) {
+        balance += amount;
+        System.out.println(amount);
+    }
+
+    void withdraw(double amount) {
+        if (amount <= balance) {
+            balance -= amount;
+            System.out.println(amount);
+        } else {
+            System.out.println("Insufficient balance");
+        }
+    }
+
+    void displayBalance() {
+        System.out.println(balance);
+    }
+}
+
+public class BankApp {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        BankAccount account = new BankAccount();
+
+        account.deposit(1000);
+        account.withdraw(500);
+        account.displayBalance();
+
+        sc.close();
+    }
+}
