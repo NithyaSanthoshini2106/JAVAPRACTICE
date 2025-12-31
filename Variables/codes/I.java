471eab4148df6411000a9fb6248c67d5c8cdc4c2
NithyaSanthoshini2106 <nithyasanthoshini099@gmail.com>
2025-12-31 09:37:34 +0530

    ok

---
+++ Variables/codes/I.java
@@ -0,0 +1,18 @@
+package 
+
+
+public class I {
+    public static void main(String[] args) {
+        String a = "madam";
+        String b = "";
+        
+        for (int i = a.length() - 1; i >= 0; i--) {
+            b += a.charAt(i);
+        }
+
+        if (a.equals(b))
+            System.out.println(a + " is a palindrome");
+        else
+            System.out.println(a + " is not a palindrome");
+    }
+}
