aa365ce00f9d323449a720b4327c25cee644dc27
NithyaSanthoshini2106 <nithyasanthoshini099@gmail.com>
2026-01-02 09:34:59 +0530

    ok

---
+++ Variables/codes/J.java
@@ -0,0 +1,19 @@
+package codes;
+
+public class J {
+
+ public static void main(String[] args) {
+        String a = "Spoorthi";
+        String b = "Nithya Raju";
+        
+        for (int i = a.length() - 1; i >= 0; i--) {
+            b += a.charAt(i);
+        }
+
+        if (a.equals(b))
+            System.out.println(a);
+        else
+            System.out.println(a);
+    }
+}
+
---
+++ Variables/iuytr.java
@@ -0,0 +1,17 @@
+public class iuytr {
+
+ public static void main(String[] args) {
+        String a = "Spoorthi";
+        String b = "Nithya Raju";
+        
+        for (int i = a.length() - 1; i >= 0; i--) {
+            b += a.charAt(i);
+        }
+
+        if (a.equals(b))
+            System.out.println(a);
+        else
+            System.out.println(a);
+    }
+}
+
