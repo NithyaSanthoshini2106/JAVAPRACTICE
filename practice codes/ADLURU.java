ef977e6749a893731565e9c97c52210afa4eb012
NithyaSanthoshini2106 <nithyasanthoshini099@gmail.com>
2026-01-03 18:53:36 +0530

    java

---
+++ practice codes/ADLURU.java
@@ -0,0 +1,22 @@
+
+    import java.util.Scanner;
+    public class ADLURU {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = sc.nextInt();
+        long factorial = 1;
+        for (int i = 1; i <= number; i++) {
+            factorial *= i;
+        }
+        System.out.println( number + factorial);
+        if (number > 20) {
+            System.out.println("Note: Factorial of large numbers may overflow long type.");
+        }
+
+        sc.close();
+    }
+}
+
+
