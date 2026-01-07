8a690b83dc9da30b8e6aaa3eff0103cc495c4a93
NithyaSanthoshini2106 <nithyasanthoshini099@gmail.com>
2026-01-07 09:37:50 +0530

    ok

---
+++ Variables/codes/unim.java
@@ -0,0 +1,25 @@
+
+
+
+
+import java.util.Scanner;
+
+public class unim {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String str = sc.nextLine();
+        String reversed = "";
+        for (int i = str.length() - 1; i >= 0; i--) {
+            reversed += str.charAt(i);
+        }
+        System.out.println(reversed);
+        if (str.equalsIgnoreCase(reversed)) {
+            System.out.println(str);
+        } else {
+            System.out.println(str);
+        }
+
+        sc.close();
+    }
+}
