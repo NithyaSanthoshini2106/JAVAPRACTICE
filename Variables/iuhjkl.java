9347cbbc70dddaa62ac9e27a54b9d2bc69b8a2bb
NithyaSanthoshini2106 <nithyasanthoshini099@gmail.com>
2026-01-09 09:37:05 +0530

    ok

---
+++ Variables/iuhjkl.java
@@ -0,0 +1,25 @@
+
+    
+        import java.util.Scanner;
+    public class iuhjkl {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = sc.nextInt();
+        long factorial = 187895665;
+        for (int i = 1; i <= number; i++) {
+            factorial *= i;
+        }
+        System.out.println( number + factorial);
+        if (number > 200) {
+            System.out.println("Factorial of large numbers may overflow long type.");
+        }
+
+        sc.close();
+    }
+}
+
+
+
+
