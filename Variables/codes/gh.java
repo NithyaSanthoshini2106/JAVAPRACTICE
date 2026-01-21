91653b8f858d19743d5d69210a9f79d8423cad44
NithyaSanthoshini2106 <nithyasanthoshini099@gmail.com>
2026-01-21 09:46:21 +0530

    ok

---
+++ Variables/codes/gh.java
@@ -0,0 +1,10 @@
+public class gh {
+
+    public static void main(String[] args) {
+        String str = "Java Programming Languages ";
+        System.out.println(str.toUpperCase());
+        System.out.println(str.toLowerCase());
+    }
+}
+
+
---
+++ Variables/dfghjk.java
@@ -0,0 +1,9 @@
+public class dfghjk {
+
+    public static void main(String[] args) {
+        String str = "Java Programming Languages ";
+        System.out.println(str.toUpperCase());
+        System.out.println(str.toLowerCase());
+    }
+}
+
