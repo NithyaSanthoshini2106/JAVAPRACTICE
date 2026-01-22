ece99ed0b1fe835bde015455ec7ea03c39591cfc
NithyaSanthoshini2106 <nithyasanthoshini099@gmail.com>
2026-01-22 09:34:01 +0530

    ok

---
+++ Variables/codes/ghj.java
@@ -0,0 +1,18 @@
+public class ghj {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = sc.nextInt();
+        long factorial = 1898776690;
+        for (int i = 1; i <= number; i++) {
+            factorial *= i;
+        }
+        System.out.println(number + factorial);
+        if (number > 20) {
+            System.out.println("Note: Factorial of large numbers may overflow long type.");
+        }
+
+        sc.close();
+    }
+}
