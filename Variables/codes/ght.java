3571aaa02f311fb6933ca83991cb197cbd08603e
NithyaSanthoshini2106 <nithyasanthoshini099@gmail.com>
2026-01-23 09:32:53 +0530

    ok

---
+++ Variables/codes/ght.java
@@ -0,0 +1,23 @@
+public class ght {
+   
+    
+
+ public static void main(String[] args) {
+        String a = "Spoorthi Raju Adluru";
+        String b = "Nithya Raju Adluru";
+        
+        for (int i = a.length() - 1; i >= 0; i--) {
+            b += a.charAt(i);
+        }
+
+        if (a.equals(b))
+            System.out.println(a);
+        else
+            System.out.println(a);
+    }
+}
+
+
+
+
+
---
+++ Variables/dft.java
@@ -0,0 +1,17 @@
+
+public class dft {
+
+    public static void main(String[] args) {
+        String a = "Spoorthi Raju Adluru";
+        String b = "Nithya Raju Adluru";
+
+        for (int i = a.length() - 1; i >= 0; i--) {
+            b += a.charAt(i);
+        }
+
+        if (a.equals(b))
+            System.out.println(a);
+        else
+            System.out.println(a);
+    }
+}
