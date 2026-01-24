ff28342fe75b92f47ca0fa03fb97426adb2e90ae
NithyaSanthoshini2106 <nithyasanthoshini099@gmail.com>
2026-01-24 10:22:28 +0530

    ok

---
+++ Variables/GradeCalculator.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class GradeCalculator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of subjects: ");
+        int n = sc.nextInt();
+
+        int[] marks = new int[n];
+        int total = 0;
+
+        for (int i = 0; i < n; i++) {
+            System.out.print("Enter marks for subject " + (i + 1) + ": ");
+            marks[i] = sc.nextInt();
+            total += marks[i];
+        }
+
+        double average = total / (double) n;
+
+        System.out.println("Total Marks: " + total);
+        System.out.println("Average: " + average);
+
+        if (average >= 90)
+            System.out.println("Grade: A");
+        else if (average >= 75)
+            System.out.println("Grade: B");
+        else if (average >= 60)
+            System.out.println("Grade: C");
+        else
+            System.out.println("Grade: Fail");
+
+        sc.close();
+    }
+}
