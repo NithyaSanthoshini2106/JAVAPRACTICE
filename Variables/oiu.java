d2ed52cf885946cc1ddfbb97fda3ebcbbfa30735
NithyaSanthoshini2106 <nithyasanthoshini099@gmail.com>
2026-01-28 09:32:02 +0530

    ok

---
+++ Variables/oiu.java
@@ -0,0 +1,35 @@
+
+
+
+class BankAccount {
+    private double balance;
+
+    BankAccount(double balance) {
+        this.balance = balance;
+    }
+
+    void deposit(double amount) {
+        balance += amount;
+        System.out.println("Deposited: " + amount);
+    }
+
+    void withdraw(double amount) {
+        if (amount <= balance) {
+            balance -= amount;
+            System.out.println("Withdrawn: " + amount);
+        } else {
+            System.out.println("Insufficient balance");
+        }
+    }
+
+    void showBalance() {
+        System.out.println("Current Balance: " + balance);
+    }
+
+    public static void main(String[] args) {
+        BankAccount acc = new BankAccount(5000);
+        acc.deposit(2000);
+        acc.withdraw(3000);
+        acc.showBalance();
+    }
+}
