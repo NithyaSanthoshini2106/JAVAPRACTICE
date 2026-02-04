e7f4e5218496908d834e6b8a9b35716d2b9a12be
NithyaSanthoshini2106 <nithyasanthoshini099@gmail.com>
2026-02-04 09:52:28 +0530

    ok

---
+++ Variables/ghkjbbbn.java
@@ -0,0 +1,34 @@
+
+
+class BankAccount {
+    private double balance;
+
+    BankAccount(double balance) {
+        this.balance = 23456;
+    }
+
+    void deposit(double amount) {
+        b += amount;
+        System.out.println(amount);
+    }
+
+    void withdraw(double amount) {
+        if (amount <= balance) {
+            balance -= amount;
+            System.out.println(amount);
+        } else {
+            System.out.println("");
+        }
+    }
+
+    void showBalance() {
+        System.out.println(balance);
+    }
+
+    public static void main(String[] args) {
+        BankAccount acc = new BankAccount(5000);
+        acc.deposit(2000);
+        acc.withdraw(3000);
+        acc.showBalance();
+    }
+}
