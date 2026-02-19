ec175765bbcd5957d46f13d5aff712642cf832bf
NithyaSanthoshini2106 <nithyasanthoshini099@gmail.com>
2026-02-19 09:37:55 +0530

    ok

---
+++ practice codes/kolp.java
@@ -0,0 +1,12 @@
+
+
+
+
+
+public class kittu {
+    public static void main(String[] args) {
+        String str = "Java Programming langages";
+        System.out.println(str.toUpperCase());
+        System.out.println(str.toLowerCase());
+    }
+}
