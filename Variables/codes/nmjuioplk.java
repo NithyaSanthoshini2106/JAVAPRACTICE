f51009321d555a1ca32f3701815d8aff22367d48
NithyaSanthoshini2106 <nithyasanthoshini099@gmail.com>
2026-02-23 09:55:50 +0530

    ok

---
+++ Variables/codes/nmjuioplk.java
@@ -0,0 +1,49 @@
+
+
+
+
+
+
+
+
+
+import java.util.Scanner;
+
+class Bank {
+    private double balance;
+
+    Bank(double balance) {
+        this.balance = balance;
+    }
+
+    void deposit(double amt) {
+        balance += amt;
+        System.out.println(amt);
+    }
+
+    void withdraw(double amt) {
+        if (amt <= balance) {
+            balance -= amt;
+            System.out.println(amt);
+        } else {
+            System.out.println("");
+        }
+    }
+
+    void showBalance() {
+        System.out.println(balance);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Bank b = new Bank(5000);
+
+        System.out.print("");
+        b.deposit(sc.nextDouble());
+        System.out.print("");
+        b.withdraw(sc.nextDouble());
+        b.showBalance();
+
+        sc.close();
+    }
+}
