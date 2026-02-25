7b812a187b891e1bd694cdbc463c713b025a4729
NithyaSanthoshini2106 <nithyasanthoshini099@gmail.com>
2026-02-25 09:48:42 +0530

    ok

---
+++ Strings/mkzasx.java
@@ -0,0 +1,32 @@
+
+
+import java.util.Scanner;
+
+public class StudentGrades {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of students: ");
+        int n = sc.nextInt();
+        String[] names = new String[n];
+        int[] marks = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            System.out.print("Enter name of student " + (i + 1) + ": ");
+            names[i] = sc.next();
+            System.out.print("Enter marks of " + names[i] + ": ");
+            marks[i] = sc.nextInt();
+        }
+
+        System.out.println("\nResults:");
+        for (int i = 0; i < n; i++) {
+            System.out.print(names[i] +marks[i] );
+            if (marks[i] >= 90) System.out.println("A");
+            else if (marks[i] >= 75) System.out.println("B");
+            else if (marks[i] >= 60) System.out.println("C");
+            else System.out.println("Fail");
+        }
+
+        sc.close();
+    }
+}
