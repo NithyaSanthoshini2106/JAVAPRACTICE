6ea42ec7d5eeb96fefd1b64b664d8979b8051ef5
NithyaSanthoshini2106 <nithyasanthoshini099@gmail.com>
2026-04-08 18:38:11 +0530

    ok

---
+++ Variables/CODESJAVA/ArrayMax.java
@@ -0,0 +1,34 @@
+
+
+
+
+
+
+
+public class ArrayMax {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter size: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            System.out.print("Enter value: ");
+            arr[i] = sc.nextInt();
+        }
+
+        int max = arr[0];
+
+        for (int i = 1; i < n; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+
+        System.out.println("Maximum: " + max);
+
+        sc.close();
+    }
+}
