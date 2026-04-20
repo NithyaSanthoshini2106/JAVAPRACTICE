b130cd2c1d86b3563eb189a243277d2a4dc098d1
NithyaSanthoshini2106 <nithyasanthoshini099@gmail.com>
2026-04-20 09:43:10 +0530

    ok

---
+++ practice codes/hgggggp09.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class ExceptionDemo {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        try {
+            System.out.print("Enter two numbers: ");
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+
+            int result = a / b;
+            System.out.println("Result: " + result);
+
+        } catch (ArithmeticException e) {
+            System.out.println("Error: Cannot divide by zero");
+        } catch (Exception e) {
+            System.out.println("Invalid input");
+        } finally {
+            System.out.println("Program ended");
+        }
+
+        sc.close();
+    }
+}
