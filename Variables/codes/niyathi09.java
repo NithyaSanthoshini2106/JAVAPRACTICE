eb4d3f770af59566bf331f7ae29b96b086a3482f
NithyaSanthoshini2106 <nithyasanthoshini099@gmail.com>
2026-05-26 15:38:19 +0530

    ok

---
+++ Variables/codes/niyathi09.java
@@ -0,0 +1,44 @@
+public class nithya {
+    import java.util.Scanner;
+
+public class niyathi09 {
+   
+
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        double total = 0;
+        char choice;
+
+        do {
+            System.out.print("Enter item name: ");
+            String item = sc.next();
+
+            System.out.print("Enter quantity: ");
+            int qty = sc.nextInt();
+
+            System.out.print("Enter price: ");
+            double price = sc.nextDouble();
+
+            double amount = qty * price;
+            total += amount;
+
+            System.out.println(item + " Amount: ₹" + amount);
+
+            System.out.print("Add more items? (y/n): ");
+            choice = sc.next().charAt(0);
+
+        } while (choice == 'y' || choice == 'Y');
+
+        System.out.println("\nTotal Bill Amount: ₹" + total);
+
+        sc.close();
+    }
+}
+
+
+
+
+}
