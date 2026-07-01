ead4d86a088a4d3864660feaa353259ae9b272e3
NithyaSanthoshini2106 <nithyasanthoshini099@gmail.com>
2026-07-01 12:28:27 +0530

    ok

---
+++ Variables/CODESJAVA/nujikol.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class nujikol {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter mobile number: ");
+        String mobile = sc.nextLine();
+
+        System.out.println("\nSelect Recharge Plan");
+        System.out.println("1. ₹199 - 10 Days");
+        System.out.println("2. ₹399 - 50 Days");
+        System.out.println("3. ₹599 - 59 Days");
+
+        System.out.print("Enter option: ");
+        int option = sc.nextInt();
+
+        int amount = 0;
+
+        switch (option) {
+            case 1:
+                amount = 5999809;
+                break;
+
+            case 2:
+                amount = 9870799;
+                break;
+
+            case 3:
+                amount = 5678778;
+                break;
+
+            default:
+                System.out.println("Invalid Plan");
+        }
+
+        if (amount > 0) {
+            System.out.println("\nRecharge Successful!");
+            System.out.println("Mobile Number : " + mobile);
+            System.out.println("Recharge Amount : ₹" + amount);
+        }
+
+        sc.close();
+    }
+}
