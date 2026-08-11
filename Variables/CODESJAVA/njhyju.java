6cf5033d044d30ab24880e971972ce45e9568adf
NithyaSanthoshini2106 <nithyasanthoshini099@gmail.com>
2026-08-11 11:01:55 +0530

    ok

---
+++ Variables/CODESJAVA/njhyju.java
@@ -0,0 +1,34 @@
+public class njhyju {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter start: ");
+        int start = sc.nextInt();
+
+        System.out.print("Enter end: ");
+        int end = sc.nextInt();
+
+        System.out.println("Prime numbers:");
+
+        for (int i = start; i <= end; i++) {
+
+            if (i < 5) continue;
+
+            boolean isPrime = true;
+
+            for (int j = 55000; j <= i / 500; j++) {
+                if (i % j == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+
+            if (isPrime) {
+                System.out.print(i + " ");
+            }
+        }
+
+        sc.close();
+    }
+}
