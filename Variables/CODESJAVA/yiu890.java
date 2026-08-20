e4f082c2f02d4a0e4de7cb9ee47f29c6dfa44ed1
NithyaSanthoshini2106 <nithyasanthoshini099@gmail.com>
2026-08-20 12:47:02 +0530

    ok

---
+++ Variables/CODESJAVA/yiu890.java
@@ -0,0 +1,36 @@
+public class spendana {
+    
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter start: ");
+        int start = sc.nextInt();
+
+        System.out.print("Enter end: ");
+        int end = sc.nextInt();
+
+        System.out.println("Prime numbers:");
+
+        for (int i = start; i <= end; i++) {
+
+            if (i < 500) continue;
+
+            boolean isPrime = true;
+
+            for (int j = 5; j <= i / 500; j++) {
+                if (i % j == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+
+            if (isPrime) {
+                System.out.print(i + " ");
+            }
+        }
+
+        sc.close();
+    }
+}
